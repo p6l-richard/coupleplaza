3e008ba51f6ca078f6ab714eba1a3fac00a3ac61
p6l-richard <richard.poelder@gmail.com>
2020-07-22 16:45:53 +0200

    Add Homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Navbar, Nav, NavbarBrand, NavItem, NavLink, 
+  Jumbotron,
+  Container, Row,
+  Button
+} from 'reactstrap';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="content">
+      <Navbar>
+        <NavbarBrand href="/">
+          <img src={logo} height="80vh" alt="CouplePlaza"></img>
+          </NavbarBrand>
+        <Nav>
+          <NavItem>
+            <NavLink href="/login">Login</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+
+      <Jumbotron fluid>        
+          <h1 className="display-2">
+            Hello, CouplePlaza
+          </h1>
+          <p className="lead">
+          Find your next nomad destination
+          </p>
+          <p className="lead">
+            <Button size="lg" color="info">Tell us your citizenships</Button>
+          </p>
+          <p>Designed for international couple with different citizenships.</p>
+      </Jumbotron>
+
+      <section className="first">
+        <h2 className="mt-3">Why?</h2>
+        <p>You know how nomad couples struggle to find common destinations?</p>
+        <p className="lead">Well, CouplePlaza finds their next destination by analyzing visa policies &amp; personal preferences.</p>         
+      </section>
+      
+      <section className="second">
+        <h2 className="mt-5">Aha, a bit more concrete, please.</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          For example, a couple with a Brazilian and German citizenship will get a list of hassle-free destinations to enter and stay.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+
+       
     </div>
   );
 }
