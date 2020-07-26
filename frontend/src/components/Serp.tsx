a08647eef10fe2402658f8aa2bd71129e15fc948
p6l-richard <richard.poelder@gmail.com>
2020-07-26 18:36:23 +0200

    ### feat: Re-design cards on SERP
    120 min
    
    - [x]  Add image to every city
    - [x]  Add relevant scores (ease of application etc.) to Card Content
        - Visa required (Y/N)
        - Ease of Visa process (hassle-free | easy | medium | difficult)
        - Visa costs
        - Duration of stay w/out Visa (30d|30d*|90d|120d|150d|>150d)
    - [x]  Add overview for 2 countries to cards

--- frontend/src/components/Serp.tsx
+++ frontend/src/components/Serp.tsx
@@ -1,29 +1,30 @@
 import * as React from 'react';
 import { 
     Container, Row, Col
-    , Card, CardBody, CardTitle
+    , Card, CardBody, CardTitle, CardImg, CardImgOverlay, CardText
 } from 'reactstrap';
 
 export interface ISerpProps {
 }
 const cities = [
-    {id: '1', name: 'Berlin', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '2', name: 'Prague', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '3', name: 'Medellin', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '4', name: 'Rio de Janeiro', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '5', name: 'Mexico City', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '6', name: 'Buenos Aires', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '7', name: 'Cuenca', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '8', name: 'Belgrade', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '9', name: 'Budapest', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '10', name: 'Bengaluru', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '11', name: 'Taipei', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '12', name: 'Phuket', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '13', name: 'Hobart', area: 'Oceania', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '14', name: 'Queenstown', area: 'Oceania', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
-    , {id: '15', name: 'Brisbane', area: 'Oceania', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    {id: '1', name: 'Germany', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: "unlimited", visaCosts:0}
+    , {id: '2', name: 'Czech Republic', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: "unlimited", visaCosts:0}
+    , {id: '3', name: 'Colombia', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 90, visaCosts:0}
+    , {id: '4', name: 'Brazil', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 90, visaCosts:0}
+    , {id: '5', name: 'Mexico', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 180, visaCosts: 22.14}
+    , {id: '6', name: 'Argentina', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 90, visaCosts: 0}
+    , {id: '7', name: 'Ecuador', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 90, visaCosts: 0}
+    , {id: '8', name: 'Serbia', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 90, visaCosts: 0}
+    , {id: '9', name: 'Hungary', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: "unlimited", visaCosts: 0}
+    , {id: '10', name: 'India', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:true, visaStay: 90, visaCosts: 68.63}
+    , {id: '11', name: 'Taiwan', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 90, visaCosts: 0}
+    , {id: '12', name: 'Thailand', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 30, visaCosts: 0}
+    , {id: '13', name: 'Indonesia', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 30, visaCosts: 0}
+    , {id: '14', name: 'Australia', area: 'Oceania', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:true, visaStay: 90, visaCosts: 0}
+    , {id: '15', name: 'New Zealand', area: 'Oceania', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:false, visaStay: 90, visaCosts: 26.80}
+    , {id: '16', name: 'Russia', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', visaIsRequired:true, visaStay: 30, visaCosts: 100}
 ]
-    
+
 
 function filterAreasOf(cities: any) {
     let areas: any = [];
@@ -40,6 +41,19 @@ function citiesFor(area: any) {
         .filter((city: any) =>  city.area == area)
 }
 
+function showVisaRequirement(visaIsRequired: boolean) {
+    if (!visaIsRequired) {
+        return "No Visa"
+    }
+    return "Visa"
+}
+
+function showVisaCosts(costs: number) {
+    return costs + " EUR"
+}
+function showStayLength(stay: number) {
+    return stay
+}
 const CARDS_PER_ROW = 4;
 
 export default class Serp extends React.Component<ISerpProps> {
@@ -63,10 +77,42 @@ export default class Serp extends React.Component<ISerpProps> {
                                     {citiesByRow.map((city: any) => {
                                         return (
                                             <Col xs="3">
-                                                <Card>
-                                                    <CardBody>
-                                                        <CardTitle>{city.name}</CardTitle>
-                                                    </CardBody>
+                                                <Card inverse>
+                                                    <CardImg width="auto" height="200vw" src={city.img}></CardImg>
+                                                    <CardImgOverlay>
+                                                        <Container>
+                                                            <Row>
+                                                            <CardTitle><h4>{city.name}</h4></CardTitle>
+
+                                                            </Row>
+                                                            <Row>
+                                                                <Col xs="1"></Col>
+                                                                <Col className="text-center"><h4>🇩🇪</h4></Col>
+                                                                <Col className="text-center"><h4>🇧🇷</h4></Col>
+                                                            </Row>
+                                                            <Row>
+                                                                <Col xs="1" >
+                                                                    <Row >🛂</Row>
+                                                                    <Row >💲</Row>
+                                                                    <Row >⌛</Row>
+                                                                </Col>
+
+                                                                <Col >
+                                                                    <Row className="d-flex justify-content-center text-center"><CardText><small>{showVisaRequirement(city.visaIsRequired)}</small></CardText></Row>
+                                                                    <Row className="d-flex justify-content-center text-center"><CardText><small>{showVisaCosts(city.visaCosts)}</small></CardText></Row>
+                                                                    <Row className="d-flex justify-content-center text-center"><CardText ><small>{showStayLength(city.visaStay)}</small></CardText></Row>
+                                                                </Col>
+                                                                <Col xs="1" className="border-left">
+                                                                </Col>
+                                                                <Col >
+                                                                    <Row className="d-flex justify-content-center text-center"><CardText className="font-weight-light"><small>{showVisaRequirement(city.visaIsRequired)}</small></CardText></Row>
+                                                                    <Row className="d-flex justify-content-center text-center"><CardText className="font-weight-light"><small>{showVisaCosts(city.visaCosts)}</small></CardText></Row>
+                                                                    <Row className="d-flex justify-content-center text-center"><CardText  className="font-weight-light"><small>{showStayLength(city.visaStay)}</small></CardText></Row>
+                                                                </Col>
+                                                            </Row>
+
+                                                        </Container>
+                                                    </CardImgOverlay>
                                                 </Card>
                                             </Col>
                                         )
