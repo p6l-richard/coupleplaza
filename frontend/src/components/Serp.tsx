bdd2e215d81700c3b96085f3fc2de2ba8d1b4b72
p6l-richard <richard.poelder@gmail.com>
2020-07-26 16:32:42 +0200

    ### feat: Update Grid Layout of SERP
    65 min
    
    - [x]  Have always display 4 Cols
        - [x]  Use `xs=3` property on Col
    - [x]  Spacing
        - [x]  Space header rows
        - [x]  Space card rows
    - [ ]  ~~**Skipped:** Refactor Code:~~
        - [ ]  ~~Understand how to make nested for loops in React more readable~~

--- frontend/src/components/Serp.tsx
+++ frontend/src/components/Serp.tsx
@@ -7,21 +7,21 @@ import {
 export interface ISerpProps {
 }
 const cities = [
-    {id: '1', name: 'Berlin', area: 'Europe'}
-    , {id: '2', name: 'Prague', area: 'Europe'}
-    , {id: '3', name: 'Medellin', area: 'Latin America'}
-    , {id: '4', name: 'Rio de Janeiro', area: 'Latin America'}
-    , {id: '5', name: 'Mexico City', area: 'Latin America'}
-    , {id: '6', name: 'Buenos Aires', area: 'Latin America'}
-    , {id: '7', name: 'Cuenca', area: 'Latin America'}
-    , {id: '8', name: 'Belgrade', area: 'Europe'}
-    , {id: '9', name: 'Budapest', area: 'Europe'}
-    , {id: '10', name: 'Bengaluru', area: 'Asia'}
-    , {id: '11', name: 'Taipei', area: 'Asia'}
-    , {id: '12', name: 'Phuket', area: 'Asia'}
-    , {id: '13', name: 'Hobart', area: 'Oceania'}
-    , {id: '14', name: 'Queenstown', area: 'Oceania'}
-    , {id: '15', name: 'Brisbane', area: 'Oceania'}
+    {id: '1', name: 'Berlin', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '2', name: 'Prague', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '3', name: 'Medellin', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '4', name: 'Rio de Janeiro', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '5', name: 'Mexico City', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '6', name: 'Buenos Aires', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '7', name: 'Cuenca', area: 'Latin America', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '8', name: 'Belgrade', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '9', name: 'Budapest', area: 'Europe', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '10', name: 'Bengaluru', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '11', name: 'Taipei', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '12', name: 'Phuket', area: 'Asia', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '13', name: 'Hobart', area: 'Oceania', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '14', name: 'Queenstown', area: 'Oceania', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
+    , {id: '15', name: 'Brisbane', area: 'Oceania', img: 'https://images.unsplash.com/photo-1571778650221-d1f5627a70ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'}
 ]
     
 
@@ -40,37 +40,45 @@ function citiesFor(area: any) {
         .filter((city: any) =>  city.area == area)
 }
 
+const CARDS_PER_ROW = 4;
+
 export default class Serp extends React.Component<ISerpProps> {
   public render() {
     return (
       <Container fluid="xs">
           {filterAreasOf(cities).map((area: any) => {
               return (
-                  <Row>
+                  <Row className="my-6">
                       <Col>
                       <h2 className="text-center">
                           {area}
                       </h2>
-                      <Row>
-
-                      {citiesFor(area).map((city: any) => {
-                          return (
-                              <Col>
-                                <Card>
-                                    <CardBody>
-                                        <CardTitle>{city.name}</CardTitle>
-                                    </CardBody>
-                                </Card>
-                              </Col>
-                          )
-                        })}
-                        </Row>
-                      </Col>
-                  </Row>
+                      { Array.from({length: Math.ceil(citiesFor(area).length / CARDS_PER_ROW)}, (x, i) => i).map(function (rowIx: any) {
+                            const start = rowIx * CARDS_PER_ROW;
+                            const end = start + CARDS_PER_ROW;
+                            const citiesByRow = citiesFor(area).slice(start, end)
+                            return (
+                                    <Row className="my-3">
+        
+                                    {citiesByRow.map((city: any) => {
+                                        return (
+                                            <Col xs="3">
+                                                <Card>
+                                                    <CardBody>
+                                                        <CardTitle>{city.name}</CardTitle>
+                                                    </CardBody>
+                                                </Card>
+                                            </Col>
+                                        )
+                                    })}
+                                    
+                                    </Row>
+                            )      
+                    })}
+                        </Col>
+                    </Row>
               )
-
-          }
-          )}
+          })}
       </Container>
     );
   }
